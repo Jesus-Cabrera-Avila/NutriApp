ec9b98125cc461f608f7459ae4ac64141fed72b6
Jesus-Cabrera-Avila <24308060610607@cetis61.edu.mx>
2025-11-04 10:11:52 -0500

    asdfg

---
+++ APP.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = "clave_secreta"
+
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
